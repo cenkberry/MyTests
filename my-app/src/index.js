23453983ae9aefc699df6cda4c263ccbd9c42053
cenkberry <cenkberi@gmail.com>
2022-03-02 08:58:34 +0300

    lets create

---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+const Page = document.getElementById('root');
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,Page
+);
+
+
+
+
+reportWebVitals();
