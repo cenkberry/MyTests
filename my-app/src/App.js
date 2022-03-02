9f22237dc665767c13bcabfb6670cdcc74f46f3c
cenkberry <cenkberi@gmail.com>
2022-03-02 09:55:27 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,15 @@
-
+const datas = {
+  id:0,
+  name: "Cenk",
+  surname: "Berry",
+  position: "Frontend Developer"
+}
 export default function App() {
   return (
-      <h1>Selam</h1>
+    <div key={datas.id}>
+      <h1>Hello!</h1>
+      <h3>My name is {datas.name} {datas.surname}</h3>
+      <h3>My Position is {datas.position}</h3>
+    </div>
   );
 }
