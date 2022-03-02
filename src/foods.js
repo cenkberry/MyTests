a633c5a42cf85958ff87092c4574bf86a9083a3b
cenkberry <cenkberi@gmail.com>
2022-03-02 20:37:02 +0300

    ok

---
+++ src/foods.js
@@ -0,0 +1,59 @@
+export const Foods = [
+  {
+    id: 1,
+    name:'CaKaBo Pizza',
+    image: 'img/pizza.jpg',
+    category: 'Pizza',
+    price:'29.99'
+  },
+  {
+    id: 2,
+    name:'Rana Cheesecake',
+    image: 'img/cheesecake.jpg',
+    category: 'Appetizer',
+    price:'6.99',
+  },
+  {
+    id: 3,
+    name:'Barkallah Wrap',
+    image: 'img/wrap.jpg',
+    category: 'Wrap',
+    price:'8.99'
+  },
+  {
+    id: 4,
+    name:'Berry Cake',
+    image: 'img/cake.jpg',
+    category: 'Dessert',
+    price:'12.99'
+  },
+  {
+    id: 5,
+    name:'Chicago Pizza',
+    image: 'img/pizza.jpg',
+    category: 'Pizza',
+    price:'29.99'
+  },
+  {
+    id: 6,
+    name:'Amsterdam Cheesecake',
+    image: 'img/cheesecake.jpg',
+    category: 'Appetizer',
+    price:'6.99',
+  },
+  {
+    id: 7,
+    name:'Berlin Wrap',
+    image: 'img/wrap.jpg',
+    category: 'Wrap',
+    price:'8.99'
+  },
+  {
+    id: 8,
+    name:'Ottoman Cake',
+    image: 'img/cake.jpg',
+    category: 'Dessert',
+    price:'12.99'
+  }
+  
+];
